d7d4c164fa2dba897fd4e381e16510985d95f4ee
Akshat Mittal <akshatmittal2506@gmail.com>
2025-12-25 10:17:28 +0530

    feat: added auth flow and middlewares

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,4 @@
+export * from "./auth";
 export * from "./enum";
 export * from "./cache";
 export * from "./http";
--- server/middlewares.ts
+++ server/middlewares.ts
@@ -1,73 +1,72 @@
-import { HTTP } from "@/constants";
+import { AuthConstants, HTTP } from "@/constants";
 import { Logger } from "@/log";
+import { AuthService } from "@/services";
 import { ApiController, ApiRequest, ApiResponse } from "@/types";
-import { SafetyUtils } from "@/utils";
-import { ApiFailure } from "./payload";
+import { SafetyUtils, StringUtils } from "@/utils";
+import { ApiFailure, ApiSuccess } from "./payload";
 
 export class ServerMiddleware {
 	public static authenticatedRoute(next: ApiController): ApiController {
 		return async (req: ApiRequest, res: ApiResponse) => {
 			try {
 				Logger.debug("authRoute -> url, cookies", req.url, req.cookies);
-				// const accessToken = StringUtils.getNonEmptyString(
-				// 	req.cookies[AuthConstants.ACCESS_TOKEN]
-				// );
-				// const refreshToken = StringUtils.getNonEmptyString(
-				// 	req.cookies[AuthConstants.REFRESH_TOKEN]
-				// );
-				// Logger.debug("Authenticating user tokens", {
-				// 	accessToken,
-				// 	refreshToken,
-				// });
-				// const authResponse = await AuthService.getAuthenticatedUser({
-				// 	accessToken,
-				// 	refreshToken,
-				// });
-				// Logger.debug("authResponse", authResponse);
-				// if (!SafetyUtils.isNonNull(authResponse)) {
-				// 	const cookies = AuthService.getCookies({
-				// 		accessToken: null,
-				// 		refreshToken: null,
-				// 		logout: true,
-				// 	});
-				// 	return new ApiFailure(res)
-				// 		.status(HTTP.status.UNAUTHORIZED)
-				// 		.message(HTTP.message.UNAUTHORIZED)
-				// 		.cookies(cookies)
-				// 		.send();
-				// }
-				// Logger.debug("Authenticated auth response", authResponse);
-				// const {
-				// 	user,
-				// 	accessToken: newAccessToken,
-				// 	refreshToken: newRefreshToken,
-				// } = authResponse;
-				// const cookies = AuthService.getUpdatedCookies(
-				// 	{ accessToken, refreshToken },
-				// 	{
-				// 		accessToken: newAccessToken,
-				// 		refreshToken: newRefreshToken,
-				// 	}
-				// );
-				// if (cookies.length > 0) {
-				// 	new ApiSuccess(res).cookies(cookies);
-				// }
-				// req.user = user;
-				// Logger.debug("Authenticated user", user);
-			} catch (error) {
-				Logger.error(error);
-				// const cookies = AuthService.getCookies({
-				// 	accessToken: null,
-				// 	refreshToken: null,
-				// 	logout: true,
-				// });
-				return (
-					new ApiFailure(res)
+				const accessToken = StringUtils.getNonEmptyString(
+					req.cookies[AuthConstants.ACCESS_TOKEN]
+				);
+				const refreshToken = StringUtils.getNonEmptyString(
+					req.cookies[AuthConstants.REFRESH_TOKEN]
+				);
+				Logger.debug("Authenticating user tokens", {
+					accessToken,
+					refreshToken,
+				});
+				const authResponse = await AuthService.getAuthenticatedUser({
+					accessToken,
+					refreshToken,
+				});
+				Logger.debug("authResponse", authResponse);
+				if (!SafetyUtils.isNonNull(authResponse)) {
+					const cookies = AuthService.getCookies({
+						accessToken: null,
+						refreshToken: null,
+						logout: true,
+					});
+					return new ApiFailure(res)
 						.status(HTTP.status.UNAUTHORIZED)
 						.message(HTTP.message.UNAUTHORIZED)
-						// .cookies(cookies)
-						.send()
+						.cookies(cookies)
+						.send();
+				}
+				Logger.debug("Authenticated auth response", authResponse);
+				const {
+					user,
+					accessToken: newAccessToken,
+					refreshToken: newRefreshToken,
+				} = authResponse;
+				const cookies = AuthService.getUpdatedCookies(
+					{ accessToken, refreshToken },
+					{
+						accessToken: newAccessToken,
+						refreshToken: newRefreshToken,
+					}
 				);
+				if (cookies.length > 0) {
+					new ApiSuccess(res).cookies(cookies);
+				}
+				req.user = user;
+				Logger.debug("Authenticated user", user);
+			} catch (error) {
+				Logger.error(error);
+				const cookies = AuthService.getCookies({
+					accessToken: null,
+					refreshToken: null,
+					logout: true,
+				});
+				return new ApiFailure(res)
+					.status(HTTP.status.UNAUTHORIZED)
+					.message(HTTP.message.UNAUTHORIZED)
+					.cookies(cookies)
+					.send();
 			}
 			return next(req, res);
 		};
@@ -83,12 +82,12 @@ export class ServerMiddleware {
 						.message(HTTP.message.UNAUTHORIZED)
 						.send();
 				}
-				// if (!AuthConstants.admins.includes(loggedInUser.email)) {
-				// 	return new ApiFailure(res)
-				// 		.status(HTTP.status.FORBIDDEN)
-				// 		.message("You are not an admin")
-				// 		.send();
-				// }
+				if (!AuthConstants.admins.includes(loggedInUser.email)) {
+					return new ApiFailure(res)
+						.status(HTTP.status.FORBIDDEN)
+						.message("You are not an admin")
+						.send();
+				}
 				req.user = loggedInUser;
 			} catch (error) {
 				Logger.error(error);
